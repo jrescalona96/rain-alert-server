02624cf520f2841a29d87ecc01f1095ad8d5b368
John Escalona <johnrichmondescalona@gmail.com>
2020-12-05 19:29:49 -0800

    Initial Commit

---
+++ src/main/java/com/jrescalona/rainalertserver/RainalertserverApplication.java
@@ -0,0 +1,13 @@
+package com.jrescalona.rainalertserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainalertserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainalertserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jrescalona/rainalertserver/RainalertserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrescalona.rainalertserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainalertserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
